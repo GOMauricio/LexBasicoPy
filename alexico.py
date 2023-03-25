312287b7d8f027461429602790ebec58407f7100
Guilherme Oliveira <102536614+GOMauricio@users.noreply.github.com>
2023-03-25 09:57:00 -0300

    Update alexico.py

--- alexico.py
+++ alexico.py
@@ -1,35 +1,37 @@
-import re #Lib que permite a criação de Expressões Regulares#
-import nltk #Lib que automatiza o processo de criação de tokens#
+import os
+import re #Lib que permite a criação de Expressões Regulares
+import sys 
+import nltk #Lib que automatiza o processo de criação de tokens
 
-p = open("Código.txt", 'r') #Recebe o arquivo e realiza a leitura do mesmo com o 'r'#
-cod = p.read() #Criação de uma nova variável que realizará a leitura do código#
-lex = nltk.wordpunct_tokenize(cod) #Variável que receberá o corpo do código, dessa vez completamente tokenizado com o wordpunct#
+p = open(os.path.join(sys.path[0],"Código.txt"), 'r') #Recebe o arquivo e realiza a leitura do mesmo com o 'r'
+cod = p.read() #Criação de uma nova variável que realizará a leitura do código
+lex = nltk.wordpunct_tokenize(cod) #Variável que receberá o corpo do código, dessa vez completamente tokenizado com o wordpunct
 
 
-print(lex, "\n") #Imprime o código inserido no arquivo, para verificar se a leitura e tokenização foram feitas da maneira correta#
+print(lex, "\n") #Imprime o código inserido no arquivo, para verificar se a leitura e tokenização foram feitas da maneira correta
 
-#Criação das Expressões Regulares e armazenamento de valores#
+#Criação das Expressões Regulares e armazenamento de valores
 
-#Palavras chaves utilizadas na linguagem criada#
+#Palavras chaves utilizadas na linguagem criada
 PR = "mostrar|natural|decimal|inserir|retornar|vazio|loop|quando|execute|selecao|etapa|parar|condinicial|condinter|condfinal|funcao"
-#Operadores utilizados na linguagem#
+#Operadores utilizados na linguagem
 OP = "(\+)|(-)|(\*)|(/)|(^)"
-#Operadores relacionais utilizados#
+#Operadores relacionais utilizados
 OP_R = "(<=)|(<)|(==)|(>)|(>=)|(!=)|(=)"
-#Expressão regular para números#
+#Expressão regular para números
 NUM = "^(\d+)$"
-#Caracteres especiais que podem a vir ser utilizados#
+#Caracteres especiais que podem a vir ser utilizados
 CS = "[\[@&~!$\\|\]:;<>?,\']|\(\)|\(|\)|{}|\[\]|\""
-#Expressão regular para definição de identificadores#
+#Expressão regular para definição de identificadores
 ID = "^[a-zA-Z_]+[a-zA-Z0-9_]*"
-#Caracteres para criação de blocos#
+#Caracteres para criação de blocos
 BL = "({)|(})"
 
-count = 0 #Contagem para possibilitar identificação de quantos tokens são utilizados#
+count = 0 #Contagem para possibilitar identificação de quantos tokens são utilizados
 for token in lex:
     count = count + 1
-    #Uso da função re.findall para identificar todas as expressões regulares no corpo do arquivo e os tokens que elas representam#
-    #Trazendo a contagem como a sua "posição", o token identificado e o que ele representa#
+    #Uso da função re.findall para identificar todas as expressões regulares no corpo do arquivo e os tokens que elas representam
+    #Trazendo a contagem como a sua "posição", o token identificado e o que ele representa
     if(re.findall(PR,token)):
         print(count,":", " ' " + token + " ' ",": Palavra Reservada" + "\n----------------------")
     elif(re.findall(OP,token)):
